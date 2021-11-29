97d6128e91589441ff06031d728e9b2fba5eafa0
park hye kyoung <lovehk67@gmail.com>
2021-11-29 14:23:10 +0900

    2021-A형 32번문제

---
+++ E2021A32.java
@@ -0,0 +1,13 @@
+public class E2021A32 {
+	private int a = 0;
+	private static int count = 0;
+	public int getA() { return a; }
+	public static int getCount() { return count; }
+	public static void main(String args[]) {
+		int sum = 0;
+		sum = a; // compile error
+		sum += count;
+		System.out.println(getA()); // compile error
+		System.out.println(getCount());
+	}
+}
