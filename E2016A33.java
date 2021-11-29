e68ce1fa39c2c69e8adcb1e61de253c638a4326e
park hye kyoung <lovehk67@gmail.com>
2021-11-29 14:37:24 +0900

    2016-A형 33번문제

---
+++ E2016A33.java
@@ -0,0 +1,34 @@
+class Parent {
+	public Parent() {
+		System.out.println("Parent default"); // 1, 7
+	}
+	public Parent(int i) {
+		this("hello");
+		System.out.println("int constructor " + i); // 4
+	}
+	public Parent(char c) {
+		this();
+		System.out.println("char constructor " + c); // 2
+	}
+	public Parent(String s) {
+		this('A');
+		System.out.println("String constructor " + s); // 3
+	}
+}
+class Child extends Parent {
+	public Child() {
+		super(10);
+		System.out.println("Child default"); // 5
+	}
+	public Child(int i) {
+		System.out.println("int constructor"); // 8
+	}
+}
+
+public class E2016A33 {
+	public static void main(String args[]) {
+		Child c1 = new Child();
+		System.out.println("-----"); // 6
+		Child c2 = new Child(10);
+	}
+}
